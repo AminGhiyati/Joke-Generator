ed70e593e6f7f4c4dd22215f75dbded263e9e26d
Amin Ghiyati <aminghiyati@MacBook-Air-von-Amin.local>
2025-02-01 15:31:32 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+})
+
+app.get("/joke", async (req, res) => {
+    try {
+        const result = await axios.get(`https://v2.jokeapi.dev/joke/Any?contains=${req.query.filter}`)
+        if (result.data.setup !== undefined) {
+            res.render("index.ejs", {
+                setup: result.data.setup,
+                delivery: result.data.delivery,
+            });
+        } else if (result.data.joke !== undefined) { // Handle single-line jokes
+            res.render("index.ejs", {
+                singleJoke: result.data.joke, // Pass joke variable to EJS
+            });
+        } else {
+            res.render("index.ejs", {
+                nojoke: true,
+            });
+        }
+        
+
+    } catch (error) {
+        
+        res.render("index.ejs", {
+            nojoke: true,
+        });
+
+
+    };
+})
+
+
+app.listen(port, () => {
+    console.log("Server is listening on port: " + port);
+});
